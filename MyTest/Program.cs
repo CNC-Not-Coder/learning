ad5c8554a0c35feeb35ea6bc8828f0e15cd9cd46
CNC-Not-Coder <limotao@qq.com>
2015-11-22 02:17:39 +0800

    Revert "Revert "excel reader""
    This reverts commit f21a54070ab1c6b17da3a0426bcbaab61e131bf5.

--- MyTest/Program.cs
+++ MyTest/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Excel;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
@@ -69,6 +71,33 @@ namespace MyTest
             var de_info = NetMessage.PB_UserInfo.Parser.ParseFrom(e.Data);
 
             Console.WriteLine("{0}:{1}:{2}", de_info.Guid, de_info.Level, de_info.NickName);
+        }
+
+        private static void InitDataTables()
+        {
+            FileStream stream = File.Open("", FileMode.Open, FileAccess.Read);
+
+            //1. Reading from a binary Excel file ('97-2003 format; *.xls)
+            //IExcelDataReader excelReader = ExcelReaderFactory.CreateBinaryReader(stream);
+
+            //2. Reading from a OpenXml Excel file (2007 format; *.xlsx)
+            IExcelDataReader excelReader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+
+            //3. DataSet - The result of each spreadsheet will be created in the result.Tables
+            //DataSet result = excelReader.AsDataSet();
+
+            //4. DataSet - Create column names from first row
+            excelReader.IsFirstRowAsColumnNames = true;
+            DataSet result = excelReader.AsDataSet();
+
+            //5. Data Reader methods
+            while (excelReader.Read())
+            {
+                //excelReader.GetInt32(0);
+            }
+
+            //6. Free resources (IExcelDataReader is IDisposable)
+            excelReader.Close();
         }
     }
 }
