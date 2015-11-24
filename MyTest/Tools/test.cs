250bd3823c4f296a8418ff610db23c16cfbb252e
CNC-Not-Coder <limotao@qq.com>
2015-11-24 11:39:12 +0800

    mcs test

---
+++ MyTest/Tools/test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyTest.Tools
+{
+    public class test
+    {
+        public void SayHello(string name)
+        {
+            Console.WriteLine("Hello, " + name);
+        }
+    }
+}
